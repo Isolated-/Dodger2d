75c48f3568b76f05fa3b8df95d4f7bcc0fc376cb
Michael Palmer <michaelpalmer53@gmail.com>
2020-02-27 21:00:32 +0000

    adds state logic to game, for restarting + pausing

--- src/game.js
+++ src/game.js
@@ -5,37 +5,74 @@ class Game {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
 
-    this.state = new State(canvas);
     this.keyboard = new Keyboard();
     this.mouse = new Mouse();
+
+    this.start();
+  }
+
+  start() {
+    this.state = new State(canvas);
+  }
+
+  clear() {
+    this.state = undefined;
+    State.SCORE = 0;
   }
 
   update(delta) {
-    if (this.state.gameOver) {
-      Game.STOPPED = true;
+    if (Mouse.isDown(Mouse.LEFT) && State.CurrentState === State.Paused) {
+      State.CurrentState = State.Running;
+      return;
     }
 
-    this.state.update(delta);
+    if (State.CurrentState === State.Stopped) {
+      State.CurrentState = State.Paused;
+      this.start();
+      return;
+    }
+
+    if (
+      State.CurrentState === State.GameOver &&
+      Keyboard.isDown(Keyboard.SPACE)
+    ) {
+      State.CurrentState = State.Stopped;
+      this.clear();
+      return;
+    }
+
+    if (State.CurrentState === State.Running && this.state) {
+      this.state.update(delta);
+    }
   }
 
   render() {
     const ctx = this.ctx;
+    ctx.textAlign = 'center';
 
-    if (this.state.gameOver) {
+    if (State.CurrentState === State.GameOver) {
       ctx.fillStyle = 'red';
+      ctx.font = '20px sans-serif';
       ctx.fillText('GAME OVER', this.canvas.width / 2, this.canvas.height / 2);
       ctx.fillText(
-        'Press F5 to restart',
+        'PRESS SPACE TO RESTART',
         this.canvas.width / 2,
         this.canvas.height / 2 + 20,
       );
       return;
     }
 
-    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    ctx.fillStyle = '#2c3e50';
-    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    ctx.fillStyle = '#ecf0f1';
 
-    this.state.render();
+    if (State.CurrentState === State.Paused) {
+      ctx.font = '16px sans-serif';
+      ctx.fillText('PAUSED', this.canvas.width / 2, this.canvas.height / 2);
+    }
+    ctx.font = '25px sans-serif';
+    ctx.fillText(`${State.SCORE}`, canvas.width / 2, 40);
+
+    if (this.state) {
+      this.state.render();
+    }
   }
 }
