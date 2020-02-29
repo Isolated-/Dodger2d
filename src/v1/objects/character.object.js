8a67d014713dfa69de27c2f48c5b7465565a203b
Michael Palmer <michaelpalmer53@gmail.com>
2020-02-29 20:48:58 +0000

    adds CharacterObject, a data class

---
+++ src/v1/objects/character.object.js
@@ -0,0 +1,30 @@
+class Character extends Dodger.GameObject {
+  static Size = 30;
+  static Colors = ['#3498db', '#f1c40f', '#c0392b'];
+  /**
+   *  @param {number[]} MaxHealth [below 50, above 50]
+   */
+  static MaxHealth = [10, 20];
+
+  /**
+   *  Creates a new Character GameObject
+   *  @param {number} x
+   *  @param {number} y
+   *  @param {boolean} direction the direction to pull the player in (false = left, true = right) (defaults to random value)
+   */
+  constructor(x, y, direction) {
+    super(
+      x,
+      y,
+      Character.Size,
+      Character.Size,
+      Dodger.GameObjectType.Character,
+      Character.Colors[0],
+    );
+
+    this.direction = direction || random(0, 2);
+    this.health = 3; // TODO: random this?
+
+    this.velocity.x = direction ? 0.8 : -0.8;
+  }
+}
