5a81e4bc66243631d7d10ebb8f08d4cfd203cb75
Michael Palmer <michaelpalmer53@gmail.com>
2020-02-29 20:48:13 +0000

    adds CharacterUpdater, responsible for movement and boundary detection

---
+++ src/v1/updaters/character.updater.js
@@ -0,0 +1,49 @@
+class CharacterUpdater extends Dodger.GameObjectUpdater {
+  constructor() {
+    super();
+    this.boundaryDetector = new Dodger.GameObjectBoundaryDetector();
+  }
+
+  update(object, delta) {
+    this._movement(object);
+    this._boundary(object);
+  }
+
+  _movement(object) {
+    if (Mouse.isDown(Mouse.LEFT)) {
+      object.position.x += object.direction
+        ? -object.velocity.x
+        : object.velocity.x;
+    }
+
+    if (Mouse.noKeys()) {
+      object.position.x += object.direction
+        ? object.velocity.x
+        : -object.velocity.x;
+    }
+  }
+
+  _boundary(object) {
+    const boundary = this.boundaryDetector.detect(
+      object,
+      CANVAS.width,
+      CANVAS.height,
+    );
+
+    const Type = Dodger.GameObjectBoundaryDetector.Type;
+
+    if (boundary !== Type.OnScreen) {
+      switch (boundary) {
+        case Type.OffScreenLeft: {
+          object.position.x = 0;
+          break;
+        }
+
+        case Type.OffScreenRight: {
+          object.position.x = CANVAS.width - object.width;
+          break;
+        }
+      }
+    }
+  }
+}
