f085ace7562791fb46656db506897002c0f0f9a1
Michael Palmer <michaelpalmer53@gmail.com>
2020-02-29 18:44:24 +0000

    fixes imports caused by directory change

--- lib/logic/generic/Updater.ts
+++ lib/logic/generic/Updater.ts
@@ -1,11 +1,11 @@
-import { IUpdater } from './IUpdater';
+import { IUpdater } from './interface/IUpdater';
 import { Vec2 } from '../../data/Vec2';
 
 export class Updater<T> implements IUpdater<T> {
   update(object: T, delta: number): void {
     throw new Error('Method not implemented.');
   }
-  movement?(from: Vec2, to: Vec2): void {
+  movement?(object: T, to: Vec2): void {
     throw new Error('Method not implemented.');
   }
 }
