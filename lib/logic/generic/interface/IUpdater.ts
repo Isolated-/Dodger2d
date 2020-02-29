10b22782275eff5b2121424ab45f3c56c5393869
Michael Palmer <michaelpalmer53@gmail.com>
2020-02-29 18:26:22 +0000

    improves IUpdater.ts movement API

--- lib/logic/generic/interface/IUpdater.ts
+++ lib/logic/generic/interface/IUpdater.ts
@@ -1,6 +1,6 @@
-import { Vec2 } from '../../data/Vec2';
+import { Vec2 } from '../../../data/Vec2';
 
 export interface IUpdater<T> {
   update(object: T, delta: number): void;
-  movement?(from: Vec2, to: Vec2): void;
+  movement?(object: T, to: Vec2): void;
 }
